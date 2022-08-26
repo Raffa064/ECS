5678e26e64eae9e0eacb51febcf67e157ed58d90
Raffa064 <raffaddtg@gmail.com>
2022-08-26 11:55:31 -0300

    Refactored, added comments, added containsLeastOne method in Entity

--- Entity.java
+++ Entity.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Entity {
-    private BinaryTree<Component> components = new BinaryTree<>();
+    private BinaryTree<Component> components = new BinaryTree<>();  //Tree of components, is a best way to improve process speed
 	private World world;
-	private int id;
+	private int id; //Entity id, can be used for get the entity in the world using getEntity() method 
 
+	//Automatically called by the World when add the entity in the World entity list
 	public void setWorld(World world) {
 		this.world = world;
 	}
@@ -16,6 +17,7 @@ public class Entity {
 		return world;
 	}
 	
+	//Returns true when constains all given Component classes
 	public boolean contains(Class<? extends Component>... componentClasses) {
 		for (Class<? extends Component> c : componentClasses) {
 			if (getComponent(c) == null) {
@@ -25,22 +27,39 @@ public class Entity {
 		return true;
 	}
 	
+	//Returns true when constains least one of the given Component classes
+	public boolean containsLeastOne(Class<? extends Component>... componentClasses) {
+		for (Class<? extends Component> c : componentClasses) {
+			if (getComponent(c) != null) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//Uses the given class to search for an component in the component tree using the class name hash code as search id
 	public <T extends Component> T getComponent(Class<T> componentClass) {
 		return (T) components.search(componentClass.getName().hashCode());
 	}
 	
+	//This method is called internally to tell World that something has been updated in the component tree.
+	public void update() {
+		if (world != null) {
+			world.onUpdateEntities(this, World.MODE_UPDATE);
+		}
+	}
+	
+	//Add an component to the entity, and call to the world
 	public void addComponent(Component component) {
 		component.entity = this;
 		components.put(component.getClass().getName().hashCode(), component);
-	}
-
-	public void update() {
-		world.onUpdateEntities(this, World.MODE_UPDATE);
+		update();
 	}
 	
+	//Remove a component from the entity, and call to the world
 	public void removeComponent(Component component) {
 		components.remove(component.getClass().getName().hashCode());
-		world.onUpdateEntities(this, World.MODE_UPDATE);
+		update();
 	}
 
 	public void setId(int id) {
--- World.java
+++ World.java
@@ -1,28 +1,26 @@
 package ecs;
 
-import java.util.List;
+import com.badlogic.gdx.Gdx;
 import java.util.ArrayList;
+import java.util.List;
 
 public class World {
-	public static final int MODE_ADD = 0;
-	public static final int MODE_REMOVE = 1;
-	public static final int MODE_UPDATE = 2;
+	//These modes are used for internal updates on ECS system 
+	public static final int MODE_ADD = 0;                       //When add a new Entity
+	public static final int MODE_REMOVE = 1;                    //When remove an Entity
+	public static final int MODE_UPDATE = 2;                    //When update the component list of an Entity
 	
-    private List<System> systems = new ArrayList<>();
-	private List<Entity> entities = new ArrayList<>();
-	private List<Entity> entitiesToRemove = new ArrayList<>();
-	private int current_max_id = 0;
-	
-	public World() {
-	}
-
-	public void setLogs(String logs) {
-	}
+    private List<System> systems = new ArrayList<>();           //List of systems for process entities
+	private List<Entity> entities = new ArrayList<>();          //List of actived entities, used for update new added systems
+	private List<Entity> entitiesToRemove = new ArrayList<>();  //List of entities to remove before the next update
+	private int current_max_id = 0;                             //Current id, increased by one for each new entity added in the World.
+	private int iterationCount;                                 //Number of system iterations on the last update, can be used for debug
 
 	public List<Entity> getEntities() {
 		return entities;
 	}
 	
+	//Search for a system by the given class
 	public <T> System getSystem(Class<T> systemType) {
 		for (int i = 0; i < systems.size(); i++) {
 			if (systems.get(i).getClass() instanceof T) {
@@ -31,7 +29,8 @@ public class World {
 		}
 		return null;
 	}
-
+	
+    //Fill the given system with entities it can process, and insert it into the World
 	public void addSystem(System system) {
 		for (Entity entity : entities) {
 			if (system.canProcess(entity)) {
@@ -41,10 +40,12 @@ public class World {
 		systems.add(system);
 	}
 
+	//Remove a system from the world
 	public void removeSystem(System system) {
 		systems.remove(system);
 	}
 
+	//Remove a system from the world that is instance of the given class
 	public <T> void removeSystemByType(Class<T> systemClass) {
 		for (int i = 0; i < systems.size(); i++) {
 			if (systems.get(i).getClass() instanceof T) {
@@ -54,6 +55,7 @@ public class World {
 		}
 	}
 	
+	//Get an entity by the ID, no longer recommended, because it generate iterations in the system
 	public Entity getEntityById(int entityId) {
 		for (int i = 0; i < entities.size(); i++) {
 			if (entities.get(i).getId() == entityId) {
@@ -63,28 +65,28 @@ public class World {
 		return null;
 	}
 
+	//Add a entity to the world after setup it
 	public void addEntity(Entity entity) {
 		entity.setWorld(this);
 		entity.setId(++current_max_id);
 		entities.add(entity);
-		onUpdateEntities(entity, MODE_ADD);
+		onUpdateEntities(entity, MODE_ADD); //Call to the system that an new entity have added, for insert it in the required systems
 	}
 
+	//Put an entity in the remove list
 	public void removeEntity(Entity entity) {
 		entitiesToRemove.add(entity);
 	}
 
+	//Call the system for each entities modification (ADD, UPDATE, and REMOVE)
 	public void onUpdateEntities(Entity e, int mode) { 
 		for (System system : systems) {
 			if (system.canProcess(e)) {
 				if (mode == MODE_ADD) {
-					java.lang.System.out.println("adding an entity to a system");
 					system.entities.add(e);
 				} else if (mode == MODE_REMOVE) {
-					java.lang.System.out.println("removing an entity to a system");
 					system.entities.remove(e);
 				} else if (mode == MODE_UPDATE) {
-					java.lang.System.out.println("updating an entity on a system");
 					system.entities.remove(e);
 					if (system.canProcess(e)) {
 						system.entities.add(e);														
@@ -94,11 +96,14 @@ public class World {
 		}
 	}
 	
+	//Called for update the ECS, it will call process() for each entity in each systems entity lists
 	public void update(float delta) {
+		iterationCount = 0;
 		for (System system : systems) {
 			system.start();
 			for (Entity entity : system.entities) {
 				system.process(this, entity, delta);
+				iterationCount++;
 			}
 			system.end();
 		}
@@ -107,5 +112,20 @@ public class World {
 			onUpdateEntities(entity, MODE_REMOVE);
 		}
 		entitiesToRemove.clear();
+	}
+	
+	//Returns the last iteration count (Number of time that process() is called)
+	public int getIterationCount() {
+		return iterationCount;
+	}
+	
+	//Number of entities in the world
+	public int getEntitiesCount() {
+		return entities.size();
+	}
+	
+	//Number of systems in the world
+	public int getSystemCount() {
+		return systems.size();
 	}
 }
